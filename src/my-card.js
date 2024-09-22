6f74cd189ba60ab2dbac4a6a9631034d65fe0130
ShyGuy2712 <jgirard356@gmail.com>
2024-09-22 17:52:03 -0400

    Added card

--- src/my-card.js
+++ src/my-card.js
@@ -2,7 +2,7 @@ import { LitElement, html, css } from 'lit';
 
 /**
  * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
+ * 1. Get your HTML from your card working in here 
  * 2. Get your CSS rescoped as needed to work here
  */
 
@@ -15,6 +15,11 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.image = "";
+    this.cardText = "";
+    this.fancy = false;
+    this.detailsButton = "Details";
+    this.detailsButtonLink = "#";
   }
 
   static get styles() {
@@ -22,16 +27,78 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      :host[fancy] {
+        background-color: black;
+        color: navy;
+        border: 16px solid navy;
+      }
+
+      detailsButton {
+        background-color: blue;
+        color: white;
+        border: none;
+        border-radius: 8px;
+        padding: 10px 20px;
+        margin: 0px 108px;
+        font-size: 0.9em;
+      }
+
+      detailsButton:focus,
+      detailsButton:hover {
+        background-color: grey;
+      }
+
+      image {
+        width: 200px;
+        height: 100%;
+      }
+
+      title {
+        position: sticky;
+        top: 0;
+        background-color: #eeeeee;
+        text-align: center;
+        font-size: 2em;
+        padding: 8px 8px 16px;
+        margin: 0 -8px;
+      }
+
+      cardText {
+        width: 300px;
+        padding: 0px 8px 8px 8px;
+        color: black;
+        background-color: white;
+        margin: 0px 0px 0px 8px;
+        height: 250px;
+        overflow: auto;
+      }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div>${this.title}</div>
+    <div>
+      <img class="image" src="${this.image}"/>
+      <div> 
+        <div class="cardText">
+          <p>${this.cardText}</p>
+          <a href=${this.detailsButtonLink}>
+            <button>${this.detailsButton}</button>
+          </a>
+        </div>
+      </div>
+    </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      cardTitle: { type: String },
+      cardText: { type: String },
+      fancy: { type: Boolean, reflect: false },
     };
   }
 }
