b44dcc6e3945bbae7078392de73074003abc293b
ShyGuy2712 <jgirard356@gmail.com>
2024-09-26 13:33:53 -0400

    Made better
    Added <description> tags to all cards, <slot> tag, implemented "fancy" class

--- src/my-card.js
+++ src/my-card.js
@@ -28,18 +28,22 @@ export class MyCard extends LitElement {
         display: block;
       }
 
-      :host[fancy] {
-        background-color: black;
-        color: navy;
-        border: 16px solid navy;
+      :host([fancy]) {
+        display: inline-block;
+        background-color: pink;
+        border: 2px solid fuchsia;
+        box-shadow: 10px 5px 5px red;
       }
 
       div {
         padding: 8px;
         margin: 8px;
         width: 300px;
+        min-height: 300px;
+        max-height: 400px;
         border-radius: 8px;
         text-align: center;
+        overflow: auto;
         background-color: white;
         border: 2px solid black;
       }
@@ -51,8 +55,10 @@ export class MyCard extends LitElement {
       img {
         margin: auto;
         display: flex;
+        aspect-ratio: 1/1;
         height: 100%;
-        width: 200px;
+        min-height: px;
+        max-width: 200px;
       }
 
       button {
@@ -70,7 +76,37 @@ export class MyCard extends LitElement {
       a {
         text-decoration: none;
       }
+
+      details summary {
+        text-align: left;
+        font-size: 20px;
+        padding: 8px 0;
+      }
+
+      details[open] summary {
+        font-weight: bold;
+      }
+
+      details div {
+        width: 264px;
+        height: 70px;
+        min-height: auto;
+        border: 2px solid black;
+        text-align: left;
+        padding: 8px;
+        overflow: auto;
+      }
     `;
+  }
+
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
 
   render() {
@@ -78,7 +114,13 @@ export class MyCard extends LitElement {
       <div>
         <img src=${this.image}>
         <h1>${this.title}</h1>
-        <p>${this.cardText}</p>  
+
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">   
+          <summary>Description</summary>
+          <div>
+            <slot>${this.cardText}</slot>
+          </div>
+        </details>
         <a href=${this.detailsButtonLink}>
         <button>${this.detailsButton}</button>
         </a>
@@ -92,7 +134,7 @@ export class MyCard extends LitElement {
       image: { type: String },
       cardTitle: { type: String },
       cardText: { type: String },
-      fancy: { type: Boolean, reflect: false },
+      fancy: { type: Boolean, reflect: true },
       detailsButton: { type: String },
       detailsButtonLink: { type: String },
     };
